accce5dfe56a61c987d3f542a126e60c2eca5fe6
apocaliptyme <anaph921@gmail.com>
2022-08-02 16:42:32 +0600

    Repeat string a given number of times. Show error if value less than 0.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
 
-};
+    if(number>=0){
+        let result = "";
+        for(let i = 0; i<number; i++){
+            result += string;
+        }
+        return result;
+
+    }else return result = "ERROR";
+}
 
 // Do not edit below this line
 module.exports = repeatString;
