1e9a27493fbcc045e0a2a967a40d46e1cdd075fe
apocaliptyme <anaph921@gmail.com>
2022-08-03 12:41:28 +0600

    Convert farenheight to celsius and vise versa,  and check for floating points and round them to 1 decimal place,   otherwise return integer.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius_temp = (5/9)*(temp - 32);
+  return ( Number.isInteger(celsius_temp) ? 
+    celsius_temp : celsius_temp.toFixed(1))*1;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let farenheight_temp = (9 * temp)/5 + 32;
+  return ( Number.isInteger(farenheight_temp) ? 
+  farenheight_temp : farenheight_temp.toFixed(1))*1;
 };
 
 // Do not edit below this line
