aadb34686523216a5592ea5574829d694efe26ed
apocaliptyme <anaph921@gmail.com>
2022-08-03 12:36:11 +0600

    Create loop to add from starting number to ending number and return result. Return error if numbers are negetive or aren't numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(Num1,Num2) {
 
+    if(typeof Num1 === "number" & typeof Num2 === "number"){
+        if(Num1<0 | Num2<0){
+            return "ERROR";
+        }else if(Num1<Num2){
+            return sum_small_to_large(Num1,Num2)
+        }else if(Num1>Num2){
+            return sum_large_to_small(Num1,Num2)
+        }
+    }else{
+        return "ERROR";
+    }
+   
 };
+
+function sum_small_to_large(startNum,endNum){
+    let result = 0;
+    for(let i = startNum; i<=endNum; i++){
+        result += i;
+    }
+    return result;
+}
+
+function sum_large_to_small(startNum,endNum){
+    let result = 0;
+    for(let i = endNum; i<=startNum; i++){
+        result += i;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
